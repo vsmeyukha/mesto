a2ddd6ba2a40a0cfcb0628fca02d4ee0a93b6ad4
vsmeyukha <vsmeyukha@gmail.com>
2020-08-11 15:13:09 +0300

    пофиксить все вопросы по JS

--- scripts/index.js
+++ scripts/index.js
@@ -2,76 +2,59 @@
 
 // * кнопка открытия попапа
 let profileEditButton = document.querySelector('.profile__edit-button');
-// console.log(profileEditButton);
 
 // * фон
 let popup = document.querySelector('.popup');
-// console.log(popup);
 
 // * кнопка закрытия попапа
 let popupCloseButton = document.querySelector('.popup__close-button');
-// console.log(popupCloseButton);
 
 // * сам попап
 let popupContainer = document.querySelector('.popup__container');
-// console.log(popupContainer);
 
+// * присваиваем переменным поля из блока profile
+let profileName = document.querySelector('.profile__name');
+let profileRegalia = document.querySelector('.profile__regalia');
+
+// * присваиваем переменным инпуты из попапа
+let popupInputTypeName = document.querySelector('.popup__input_type_name');
+let popupInputTypeRegalia = document.querySelector('.popup__input_type_regalia');
+
+let popupForm = popup.querySelector('.popup__form');
+
+// * объявляем функцию, которая вставляет и удаляет из HTML класс popup_opened
 let changePopupClass = function () {
   popup.classList.toggle('popup_opened');
 }
 
+// * объявляем функцию, которая закрывает попап по клику в любое место на экране, кроме самого попапа (класс эл-та popup__container)
 let popupClose = function (event) {
   if (event.target !== event.currentTarget) {
     return;
-  } else {
-    changePopupClass(event);
-  }
+  } changePopupClass(event);
 }
-
-profileEditButton.addEventListener('click', changePopupClass);
-popupCloseButton.addEventListener('click', changePopupClass);
-popup.addEventListener('click', popupClose)
-
-let page = document.querySelector('.page');
-
-// page.addEventListener('click', (evt) => console.log('gogol'), true);
-
-// ! ОТОБРАЖЕНИЕ ИНФЫ СО СТРАНИЦЫ В ПЛЕЙСХОЛДЕРАХ ФОРМ ПОПАПА
-
-let profileName = document.querySelector('.profile__name');
-let profileRegalia = document.querySelector('.profile__regalia');
-
-let popupInputName = document.querySelector('.popup__input-name');
-let popupInputRegalia = document.querySelector('.popup__input-regalia');
-
-popupInputName.placeholder = profileName.textContent;
-popupInputRegalia.placeholder = profileRegalia.textContent;
-
-console.log(popupInputName.value);
-
-
 
 // ! ЗНАЧЕНИЯ, ВВЕДЕННЫЕ В ФОРМЫ, ПЕРЕДАЮТСЯ В БЛОК PROFILE
 
-let popupForm = popup.querySelector('.popup__form');
-
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
-  
-  let popupInputName = document.querySelector('.popup__input-name');
-  let popupInputRegalia = document.querySelector('.popup__input-regalia');
-  
-  let profileName = document.querySelector('.profile__name');
-  let profileRegalia = document.querySelector('.profile__regalia');
 
-  let popupInputNameValue = popupInputName.value;
-  let popupInputRegaliaValue = popupInputRegalia.value;
+  profileName.textContent = popupInputTypeName.value;
+  profileRegalia.textContent = popupInputTypeRegalia.value;
 
-  profileName.textContent = popupInputNameValue;
-  profileRegalia.textContent = popupInputRegaliaValue;
+  // ! Ирина, я не понял, что вы имеете в виду, говоря, что "данные должны заноситься в форму в момент открытия модального окна". поясните, пожалуйста. 
+  popupInputTypeName.placeholder = profileName.textContent;
+  popupInputTypeRegalia.placeholder = profileRegalia.textContent;
+
+  changePopupClass();
 }
 
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+// * навешиваем обработчики событий на кнопку edit в блоке profile и кнопку закрытия открытого попапа. по клику на эти эл-ты запускается функция changePopupClass - добавление или удаление класса popup_opened
+profileEditButton.addEventListener('click', changePopupClass);
+popupCloseButton.addEventListener('click', changePopupClass);
+
+// * навешиваем обработчик событий на фон попапа. по клику на фон попап закрывается.
+popup.addEventListener('click', popupClose);
 popupForm.addEventListener('submit', formSubmitHandler);
-popupForm.addEventListener('submit', changePopupClass);
+
+// ! Ирина, надеюсь, вы досюда дочитаете) не пойму один нюанс: когда вводишь что-то в формы в попапе, сохраняешь, закрываешь и открывааешь попап снова, эти значения сохраняются в формах, то есть они не в плейсхолдерах, а в value. почему так происходит и как это исправить? 
