13c129c0c3a1651b1a53772b80df7722f36079f4
vsmeyukha <vsmeyukha@gmail.com>
2020-08-28 11:20:36 +0300

    feat: сделать добавление новой карточки по клику на плюсик в профиле

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,7 @@ const popupTypeProfileEdit = document.querySelector('.popup_type_profile-edit');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const popupInputTypeName = document.querySelector('.popup__input_type_name');
 const popupInputTypeRegalia = document.querySelector('.popup__input_type_regalia');
-const popupForm = popupTypeProfileEdit.querySelector('.popup__form');
+const popupFormTypeUserInfo = popupTypeProfileEdit.querySelector('.popup__form_type_user-info');
 
 // * объявляем функцию, которая вставляет и удаляет из HTML класс popup_opened
 const togglePopupClass = function () {
@@ -43,30 +43,9 @@ popupCloseButton.addEventListener('click', togglePopupClass);
 
 // * навешиваем обработчик событий на фон попапа. по клику на фон попап закрывается.
 popupTypeProfileEdit.addEventListener('click', closePopupOnClick);
-popupForm.addEventListener('submit', formSubmitHandler);
+popupFormTypeUserInfo.addEventListener('submit', formSubmitHandler);
 
-// ! открытие и закрытие попапа добавления карточек
-
-const profileAddButton = document.querySelector('.profile__add-button');
-const popupTypeAddNewCard = document.querySelector('.popup_type_add-new-card');
-const anotherCloseButton = document.querySelector('.another-close-button');
-
-const togglePopup = () => {
-  popupTypeAddNewCard.classList.toggle('popup_opened');
-}
-
-// ! закрытие попапа по клику в любое место
-const closePopupOnPopupClick = (evt) => {
-  if (evt.currentTarget === evt.target) {
-    togglePopup(evt);
-  } return;
-}
-
-profileAddButton.addEventListener('click', togglePopup);
-anotherCloseButton.addEventListener('click', togglePopup);
-popupTypeAddNewCard.addEventListener('click', closePopupOnPopupClick);
-
-// ! рендеринг карточек
+// ! массив карточек
 
 const initialCards = [
   {
@@ -92,14 +71,91 @@ const initialCards = [
   {
       name: 'Байкал',
       link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  },
-  {
-    name: 'Mozel',
-    link: 'https://alcoholclub.ru/wp-content/uploads/2019/02/Pivo-kozel-Kozel-37.jpg'
   }
 ];
 
+// ! открытие и закрытие попапа добавления карточек
+
+const profileAddButton = document.querySelector('.profile__add-button');
+const popupTypeAddNewCard = document.querySelector('.popup_type_add-new-card');
+const anotherCloseButton = document.querySelector('.another-close-button');
+
+const togglePopup = () => {
+  popupTypeAddNewCard.classList.toggle('popup_opened');
+}
+
+// ! закрытие попапа по клику в любое место
+const closePopupOnPopupClick = (evt) => {
+  if (evt.currentTarget === evt.target) {
+    togglePopup(evt);
+  } return;
+}
+
+profileAddButton.addEventListener('click', togglePopup);
+anotherCloseButton.addEventListener('click', togglePopup);
+popupTypeAddNewCard.addEventListener('click', closePopupOnPopupClick);
+
+// ! добавление карточки
+
+// ? создаем одну ф-ию, чтобы прокидывать значения из форм в массив. в этой же ф-ии первой строкой превент дефолт. параметр ф-ии evt - не то же, что card для взаимодействия с массивом. а в конце ф-ии вызывается ф-ия тоггл попап. дальше навешиваем обработчик на форму второго попапа, параметром ей передается вот эта первая ф-ия. 
+
+// const addACard = function () {
+
+//   const card = {
+//       name: cardTitle.value,
+//       link: cardLink.value
+//   }
+//   return card;
+// }
+
+// const addToForm = function (evt) {
+//   evt.preventDefault();
+
+//   initialCards.unshift(addACard());
+
+//   togglePopup();
+// }
+
+// popupFormTypeAddCard.addEventListener('submit', addToForm);
+
+// const testconsole = function () {
+  
+//   const card = {
+//     name: profileName.textContent,
+//     link: 'https://images.unsplash.com/photo-1597287258659-0b6391c0c5f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2978&q=80'
+//   }
+//   return card;
+// }
+// initialCards.unshift(testconsole());
+
+// initialCards.unshift({ name: 'oloооlo', link: 'https://images.unsplash.com/photo-1598377706489-bb127e4ce363?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80' });
+
+// initialCards[1] = { name: 'zalupa', link: 'https://images.unsplash.com/photo-1597287258659-0b6391c0c5f5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2689&q=80' } // вот так обращаться к св-вам объекта, являющегося элементом массива
+
+// ! создание новой карточки
+
+const popupFormTypeAddCard = document.querySelector('.popup__form_type_add-card');
+const inputCardTitle = document.querySelector('.popup__input_type_card-title');
+const inputCardLink = document.querySelector('.popup__input_type_card-link');
 const cardsSection = document.querySelector('.cards');
+
+const addACard = evt => {
+  evt.preventDefault();
+  
+  const cardTemplate = document.querySelector('#cards-template').content.cloneNode(true);
+
+  cardTemplate.querySelector('.card__title').textContent = inputCardTitle.value;
+  cardTemplate.querySelector('.card__img').src = inputCardLink.value;
+
+  cardsSection.prepend(cardTemplate);
+  
+  togglePopup();
+}
+
+popupFormTypeAddCard.addEventListener('submit', addACard);
+
+
+// ! рендеринг карточек
 
 const addCards = card => {
   const cardTemplate = document.querySelector('#cards-template').content.cloneNode(true);
